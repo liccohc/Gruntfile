84c360cd5f28e93f64c51db548122bb5f2d11bab
liccohc <155593419+liccohc@users.noreply.github.com>
2024-06-27 18:53:09 -0300

    Add files via upload

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            development: {
+                files:{
+                    'main.css':'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'main.min.js': 'main.js'
+                }
+            }
+        }
+    })
+
+    grunt.registerTask('olaGrunt', function() {
+        const done = this.async();
+        setTimeout(function(){
+            console.log('Olá Grunt');
+            done();
+        }, 3000);
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+    grunt.registerTask('default',['less']);
+    grunt.registerTask('build',['uglify']);
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMcontentLoaded',function(){
+
+})
+
+var str = 'Terminal Root';
+
+if(str.match(/Root/)){
+    document.write('Encontrou');
+}else{
+	document.write('Palavra Não Encontrada');
+}
